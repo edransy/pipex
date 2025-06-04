aa99c340236ab393a5b41d5e0119faea74560d67
edransy <75527882+edransy@users.noreply.github.com>
2025-06-04 14:35:48 +0200

    Delete pipex-macros/target/package directory

--- pipex-macros/target/package/pipex-macros-0.2.0/target/debug/deps/pipex_macros-230b567ff18d55c0.d
+++
@@ -1,5 +0,0 @@
-/Users/dransy/Documents/pipex/pipex-macros/target/package/pipex-macros-0.2.0/target/debug/deps/libpipex_macros-230b567ff18d55c0.dylib: src/lib.rs
-
-/Users/dransy/Documents/pipex/pipex-macros/target/package/pipex-macros-0.2.0/target/debug/deps/pipex_macros-230b567ff18d55c0.d: src/lib.rs
-
-src/lib.rs:
